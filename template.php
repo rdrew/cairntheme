34860b75f9e34036b1eb1ae0c42808882fad100a
rdrew <robdrewupei@gmail.com>
2015-01-30 14:17:13 -0400

    1st commit

---
+++ template.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Override or insert variables for the html template.
+ */
+/* -- Delete this line if you want to use this function
+function cairntheme_preprocess_html(&$vars) {
+}
+function cairntheme_process_html(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables for the page templates.
+ */
+/* -- Delete this line if you want to use these functions
+function cairntheme_preprocess_page(&$vars) {
+}
+function cairntheme_process_page(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the node templates.
+ */
+/* -- Delete this line if you want to use these functions
+function cairntheme_preprocess_node(&$vars) {
+}
+function cairntheme_process_node(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the comment templates.
+ */
+/* -- Delete this line if you want to use these functions
+function cairntheme_preprocess_comment(&$vars) {
+}
+function cairntheme_process_comment(&$vars) {
+}
+// */
+
+
+/**
+ * Override or insert variables into the block templates.
+ */
+/* -- Delete this line if you want to use these functions
+function cairntheme_preprocess_block(&$vars) {
+}
+function cairntheme_process_block(&$vars) {
+}
+// */
